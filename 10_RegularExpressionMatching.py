b513d5b6fd8563c20e02d1b1804ed49141d48bdb
Adrian Clinansmith <adrianclinansmith@hotmail.com>
2022-11-30 23:14:34 -0500

    Added problem description

--- 10_RegularExpressionMatching.py
+++ 10_RegularExpressionMatching.py
@@ -1,3 +1,12 @@
+"""
+10. Regular Expression Matching
+
+Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+- '.' Matches any single character.​​​​
+- '*' Matches zero or more of the preceding element.
+The matching should cover the entire input string (not partial).
+"""
+
 def handleStar(s: str, si: int, p: str, pi: int) -> bool:
     c = p[pi] # extract the character
     p = p[pi + 2 : ] # consume star expression
