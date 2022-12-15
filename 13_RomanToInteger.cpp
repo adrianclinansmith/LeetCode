18d0785a5114ee1a66ce2eed6c94054486a92e73
Adrian Clinansmith <adrianclinansmith@hotmail.com>
2022-12-14 21:20:22 -0500

    added compile commands

--- 13_RomanToInteger.cpp
+++ 13_RomanToInteger.cpp
@@ -28,18 +28,20 @@ Constraints:
 - It is guaranteed that s is a valid roman numeral in the range [1, 3999].
 */
 
+#include <array>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-string symbols[] = {"M", "CM", "D", "CD", "C", "XC", 
+const array<string, 13> symbols = {"M", "CM", "D", "CD", "C", "XC", 
         "L", "XL", "X", "IX", "V", "IV", "I"};
-int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+const array<int, 13> values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 
 class Solution {
 public:
-    int romanToInt(string s) {
-        for (int i = 0; i < 13; i++) {
+    int romanToInt(string const& s) const {
+        for (int i = 0; i < symbols.size(); i++) {
             cout << symbols[i] << "\n";
         }
         return 0;
