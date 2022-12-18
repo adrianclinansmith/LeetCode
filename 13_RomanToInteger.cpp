e7abf2cfd33aed74c573217035dd6ed205e7b023
Adrian Clinansmith <adrianclinansmith@hotmail.com>
2022-12-18 10:59:35 -0500

    Cleaned up code

--- 13_RomanToInteger.cpp
+++ 13_RomanToInteger.cpp
@@ -30,13 +30,8 @@ Constraints:
 
 #include <iostream>
 #include <string>
-#include <unordered_map>
 
 using namespace std;
-
-// unordered_map<string, int> const symbolMap = {
-//     {"M", 1000}, {"CM", 900}, {"D", 500}, {"CD", 400}, {"C", 100}, {"XC", 90}, {"L", 50}, {"XL", 40}, {"X", 10}, {"IX", 9}, {"V", 5}, {"IV", 4}, {"I", 1}
-// };
 
 class Solution {
 public:
