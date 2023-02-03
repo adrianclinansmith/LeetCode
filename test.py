88f7caaf1f78a08fe75e64062d5455dea6d0e838
Adrian Clinansmith <adrianclinansmith@hotmail.com>
2023-02-02 20:50:02 -0500

    refactor

--- test.py
+++ test.py
@@ -3,25 +3,13 @@
 a?bcd?a?
 """
 
-def solution(riddle: str) -> str:
-    s = ""
-    for i in range(len(riddle)):
-        if riddle[i] != "?":
-            s += riddle[i]
-            continue
-        c1 = "a" if i == 0 else s[-1]
-        c2 = "a" if i == len(riddle) - 1 else riddle[i+1]
-        if c1 != "a" and c2 != "a":
-            s += "a"
-        elif c1 != "b" and c2 != "b": 
-            s += "b"
-        else:
-            s += "c"
-    return s
+# 0 -> 1, 1 -> 3, 2 -> 5, -> 3 -> 7, 4 -> 9
 
-def test(s: str):
-    print(f"{s} -> {solution(s)}")
 
-test("a?bcd?a?")
-test("?")
-test("????")
+class MinHeap:
+    def __init__(self):
+        self.list = list()
+    def add(self, entry):
+        self.list[2 * len(self.list) + 1] = entry
+minHeap = list()
+
